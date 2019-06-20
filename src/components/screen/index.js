7eea745b37ad8da819661a6bfc30a46dace03749
Devin Canterberry <devin@canterberry.cc>
2019-06-19 18:19:37 -0700

    🎨 Add textured background and depth to intro screens

--- src/components/screen/index.js
+++ src/components/screen/index.js
@@ -5,13 +5,18 @@ const Screen = styled.div`
   align-items: center;
   background-color: ${theme('color', 'primary', 'background')};
   background: ${theme('gradient', 'screen')};
+  border-radius: 4px;
+  box-shadow: 2px 2px 1px rgba(0, 0, 0, 0.5);
   color: ${theme('color', 'primary', 'foreground')};
   display: flex;
   flex: 1;
   flex-direction: column;
   font: ${theme('typeface', 'normal')};
+  height: 80%;
   overflow: hidden;
+  margin: 32px auto;
   padding: ${theme('space', 'large')};
+  width: 80%;
 `;
 
 export default Screen;
