9bcb0e9ef434e1883aa464d3ac5d5027d21ba98c
Devin Canterberry <devin@canterberry.cc>
2019-08-30 08:09:02 -0700

    🐛 Fix the Invite dialog crashing the app
    Wire the Invite to Group dialog into the Redux state.
    The dialog is stil "inactive" -- committing it does not yet do
    anything, but the UI now collects all necessary details for
    creating a new group.

--- src/dialogs/groups-dialog/index.js
+++ src/dialogs/groups-dialog/index.js
@@ -1,7 +1,7 @@
 import Dialog from '/components/dialog';
 import PropTypes from 'prop-types';
 import React from 'react';
-import TextInput from '/components/text-input';
+import Select from 'react-select';
 
 import { connect } from 'react-redux';
 import styled from 'styled-components';
@@ -13,62 +13,185 @@ const Content = styled.div`
   flex-direction: column;
 `;
 
-const Introduction = styled.div`
-  margin-bottom: ${theme('space', 'normal')};
+const Field = styled.div`
+  align-items: stretch;
+  display: flex;
+  flex-direction: column;
+  margin: 0 0 ${theme('space', 'large')};
 `;
 
-const UsersPicker = styled.div`
-  padding: ${theme('space', 'normal')} 0;
+const Description = styled.div`
+  margin: ${theme('space', 'normal')} 0;
 `;
 
-const UsersPickerChoice = styled.div`
-  background-color: ${theme('highlight', 'low')};
-  border-radius: ${theme('space', 'normal')};
-  display: inline-block;
-  margin: 0 ${theme('space', 'normal')} ${theme('space', 'normal')} 0;
-  padding: ${theme('space', 'normal')};
-`;
-
-const UsersPickerSearch = styled(TextInput)`
-  font-size: inherit;
+const StyledTextInput = styled.input`
+  background: none;
+  border-color: ${theme('color', 'action', 'borderInactive')};
+  border-style: solid;
+  border-width: 0 0 2px;
+  color: inherit;
   margin: 0;
-  padding: ${theme('space', 'small')};
+  padding: ${theme('space', 'normal')};
   width: auto;
+
+  ${(props) => props.theme.transition('border-color')}
+
+  :active,
+  :focus,
+  :hover {
+    border-color: ${theme('color', 'action', 'border')};
+  }
+
+  :active:hover,
+  :focus:hover {
+    border-color: ${theme('color', 'action', 'border')};
+  }
 `;
 
-const GroupsDialog = ({ history, peer }) => <Dialog
-  commitLabel="Create"
-  content={<Content>
-    <Introduction>Start a new conversation with...</Introduction>
-    <UsersPicker>
-      <UsersPickerChoice>You</UsersPickerChoice>
-      <UsersPickerChoice>{peer.name}</UsersPickerChoice>
-    </UsersPicker>
-    <UsersPickerSearch isAutoFocus placeholder="Invite others..."/>
-  </Content>}
-  onCancel={() => history.goBack()}
-  onCommit={() => history.goBack()}
-  title="Create a group"
-/>;
+class TextInput extends React.Component {
+  static get propTypes() {
+    const { bool } = PropTypes;
+    return {
+      ...StyledTextInput.propTypes || {},
+      isAutoFocus: bool
+    };
+  }
+  handleChange = (event) => {
+    this.props.onChange(event.target.value);
+  };
 
-const { func, shape, string } = PropTypes;
+  render() {
+    const { props: { isAutoFocus = false } } = this;
+    return <StyledTextInput {...this.props} autoFocus={isAutoFocus} onChange={this.handleChange}/>;
+  }
+}
 
-GroupsDialog.propTypes = {
-  history: shape({ goBack: func.isRequired }).isRequired,
-  location: shape({
-    state: shape({
-      peerId: string.isRequired
-    }).isRequired
-  }).isRequired,
-  onCommit: func.isRequired,
-  peer: shape({
-    name: string.isRequired
-  }).isRequired
-};
+class GroupsDialog extends React.Component {
+  static get propTypes() {
+    const { arrayOf, func, shape, string } = PropTypes;
+
+    return {
+      history: shape({ goBack: func.isRequired }).isRequired,
+      location: shape({
+        state: shape({
+          topicId: string.isRequired
+        }).isRequired
+      }).isRequired,
+      onCommit: func.isRequired,
+      peers: shape({
+        default: arrayOf(shape({
+          attributes: shape({
+            displayName: string.isRequired
+          }).isRequired,
+          id: string.isRequired
+        })).isRequired,
+        eligible: arrayOf(shape({
+          attributes: shape({
+            displayName: string.isRequired
+          }).isRequired,
+          id: string.isRequired
+        })).isRequired
+      }).isRequired
+    };
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      groupName: '',
+      peerSelection: []
+    };
+  }
+
+  handleGroupNameChange = (groupName) => this.setState({ groupName });
+
+  handlePeerSelectionChange = (peerSelection) => {
+    this.setState({ peerSelection });
+  }
+
+  render() {
+    const {
+      handleGroupNameChange,
+      handlePeerSelectionChange,
+      props: { history, onCommit, peers },
+      state: { groupName, peerSelection }
+    } = this;
+    const options = peers.eligible.map((peer) => ({
+      isSelected: peerSelection.some((it) => it.value === peer),
+      label: peer.attributes.displayName,
+      value: peer
+    }));
+    const defaults = options.filter((it) => peers.default.includes(it.value));
+
+    return <Dialog
+      commitLabel="Create"
+      content={<Content>
+        <Field>
+          <Select
+            defaultValue={defaults}
+            isMulti
+            onChange={handlePeerSelectionChange}
+            options={options}
+            placeholder="Invite others..."
+            styles={{
+              option: (provided, state) => ({
+                ...provided,
+                color: state.theme.colors.neutral70
+              })
+            }}
+          />
+          <Description>The people you select will be invited to join the group.</Description>
+        </Field>
+        <Field>
+          <TextInput isAutoFocus onChange={handleGroupNameChange} placeholder="Name the group..." value={groupName}/>
+          <Description>This group name will be visible to all invitees.</Description>
+        </Field>
+      </Content>}
+      onCancel={() => history.goBack()}
+      onCommit={onCommit}
+      title="Create a group"
+    />;
+  }
+}
 
 export { GroupsDialog };
 
-export default connect((state, props) => ({
-  peer: state.indexes.peers.byId[props.location.state.peerId]
-}), () => ({
+export default connect((state, props) => {
+  const {
+    indexes: {
+      resources: {
+        by: {
+          type: {
+            identity: identities,
+            self: [
+              {
+                relationships: {
+                  identity: {
+                    id: selfId
+                  }
+                }
+              }
+            ],
+            topicMembership: topicMemberships
+          }
+        }
+      }
+    }
+  } = state;
+  const { location: { state: { topicId } } } = props;
+  const memberships = topicMemberships.filter((it) => it.relationships.topic.id === topicId);
+
+  return {
+    peers: identities.reduce((a, identity) => {
+      if (identity.id !== selfId) {
+        a.eligible.push(identity);
+        if (memberships.some((it) => it.relationships.identity.id === identity.id)) {
+          a.default.push(identity);
+        }
+      }
+      return a;
+    }, { default: [], eligible: [] })
+  };
+}, (dispatch, props) => ({
+  onCommit: () => props.history.goBack()
 }))(GroupsDialog);
